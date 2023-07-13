813cda51fe9c34fd1ef4f0cf6ad926251f00ad81
Duy Nguyen <nhdduy67@gmail.com>
2023-07-13 15:07:52 +0700

    Finish 02 client and server

--- 01_client.py
+++ 01_client.py
@@ -1,16 +1,16 @@
 import socket
 
-HOST = "127.0.0.1"  #IP address server
-PORT = 65432        #port is used by the server
+HOST = "127.0.0.1"  # IP address server
+PORT = 65432  # port is used by the server
 
-client= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = (HOST, PORT)
 print("Client connect to server with port: " + str(PORT))
 client.connect(server_address)
 
 try:
     while True:
-        msg=input('Client: ')
+        msg = input('Client: ')
         client.sendall(bytes(msg, "utf8"))
 except KeyboardInterrupt:
     client.close()
