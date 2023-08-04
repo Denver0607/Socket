e8d7de3ffc39f41f044852d8989686901bf81633
Duy Nguyen <nhdduy67@gmail.com>
2023-08-04 18:28:12 +0700

    Add source code for GET, POST, HEAD

--- proxy.py
+++ proxy.py
@@ -1,211 +1,206 @@
 import socket
 import threading
 import os
-import time
-import configparser
+import datetime
+from urllib.parse import urlparse
 
-# Define the proxy server IP and port
-proxy_ip = "127.0.0.1"  # Change this to the appropriate IP if the proxy is running on a different machine
-proxy_port = 8888       # Change this to the appropriate port used by your proxy server
+CACHE_DIRECTORY = "cache"
+CONFIG_FILE = "config.ini"
+FORBIDDEN_PAGE = "403.html"
+NOT_FOUND_PAGE = "404.html"
 
-# Define global variables to store configuration values
-CACHE_EXPIRATION_TIME = 0
-ALLOWED_START_TIME = 0
-ALLOWED_END_TIME = 0
-whitelisted_domains = []
+def receive_data(client_socket):
+    data = b""
+    while True:
+        chunk = client_socket.recv(4096)
+        if not chunk:
+            break
+        data += chunk
+    return data
 
-# Define the cache directory
-CACHE_DIR = "cache"
-if not os.path.exists(CACHE_DIR):
-    os.makedirs(CACHE_DIR)
-
-# Load configurations from config file
-def load_config():
-    config = configparser.ConfigParser()
-    config.read("config.txt")
-
-    # Read CACHE_EXPIRATION_TIME from the config file
-    global CACHE_EXPIRATION_TIME
-    CACHE_EXPIRATION_TIME = int(config.get('CACHE', 'cache_time').strip())
-
-    # Read ALLOWED_START_TIME and ALLOWED_END_TIME from the config file
-    global ALLOWED_START_TIME, ALLOWED_END_TIME
-    time_range = config.get('CACHE', 'time').strip().split('-')
-    ALLOWED_START_TIME = int(time_range[0])
-    ALLOWED_END_TIME = int(time_range[1])
-
-    # Read whitelisted_domains from the config file
-    global whitelisted_domains
-    whitelisted_domains = [domain.strip() for domain in config.get('CACHE', 'whitelisting').split(',')]
-
-# Load the cache from file (for persistent caching)
-def load_cache():
-    cached_objects = {}
-    for cached_file in os.listdir(CACHE_DIR):
-        file_path = os.path.join(CACHE_DIR, cached_file)
-        url = os.path.basename(file_path)
-
-        with open(file_path, 'rb') as f:
-            cached_data = f.read()
+class ProxyHandler:
+    def __init__(self, client_socket, client_address):
+        self.client_socket = client_socket
+        self.client_address = client_address
         
-        cached_objects[url] = cached_data
+    def handle_request(self):
+        request = receive_data(self.client_socket)
+        method, url = self.parse_request(request)
+        content_length = self.get_content_length(request)
 
-    return cached_objects
+        if method and url and self.is_whitelisted(url) and self.is_time_allowed():
+        # if method:
+            if method == "GET":
+                self.handle_get_request(url)
+            elif method == "POST":
+                self.handle_post_request(url, request, content_length)
+            elif method == "HEAD":
+                self.handle_head_request(url)
+            else:
+                # Unsupported method, return 403 Forbidden
+                self.send_forbidden_response()
+        else:
+            # URL not whitelisted or access not allowed, return 403 Forbidden
+            self.send_forbidden_response()
 
-# Save the cache to file (for persistent caching)
-def save_cache(url, data):
-    # Create the file path for the cached object
-    cache_file_path = os.path.join(CACHE_DIR, url)
+        self.client_socket.close()
 
-    # Write the data to the cache file
-    with open(cache_file_path, 'wb') as f:
-        f.write(data)
+    def parse_request(self, request):
+        lines = request.split(b"\r\n")
+        if not lines:
+            return None, None
 
-# Check if a URL is whitelisted
-def is_whitelisted(url):
-    # Your code to check if the URL is whitelisted here...
-    for domain in whitelisted_domains:
-        if domain in url:
-            return True
-    return False
+        # Extract HTTP method and URL from the first line of the request
+        method, url, _ = lines[0].split(b" ")
 
-# Function to fetch an image from the web server
-def fetch_image_from_web_server(url):
-    try:
-        # Parse the URL to get the host and path
-        _, _, host, path = url.split('/', 3)
+        return method.decode(), url.decode()
 
-        # Open a socket connection to the web server
-        web_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        web_socket.connect((host, 80))
 
-        # Create the HTTP request to fetch the image
-        request = f"GET /{path} HTTP/1.1\r\nHost: {host}\r\n\r\n"
+    def get_content_length(self, request):
+        content_length_header = b"Content-Length: "
+        index = request.find(content_length_header)
+        if index != -1:
+            end_index = request.find(b"\r\n", index + len(content_length_header))
+            content_length = request[index + len(content_length_header):end_index].strip()
+            return int(content_length)
+        return 0
 
-        # Send the request to the web server
-        web_socket.sendall(request.encode())
+    def receive_request_body(self, content_length):
+        # Receive the request body with the given content length
+        return self.client_socket.recv(content_length)
 
-        # Receive the response from the web server
-        response = b""
-        while True:
-            chunk = web_socket.recv(4096)
-            if not chunk:
-                break
-            response += chunk
+    def is_whitelisted(self, url):
+        # Implement whitelisting logic from the config file
+        # Return True if URL is whitelisted, otherwise False
+        return True
 
-        # Close the socket connection
-        web_socket.close()
+    def is_time_allowed(self):
+        # Implement time-based access restrictions from the config file
+        # Return True if access is allowed, otherwise False
+        return True
+    
+    def handle_get_request(self, url):
+        cached_data = self.get_cached_data(url)
+        if cached_data:
+            # Send cached data back to the client
+            self.send_cached_response(cached_data)
+        else:
+            # Forward the GET request to the web server
+            response = self.forward_request("GET", url, b"")
+            if response:
+                if response.startswith(b"HTTP/1.1 404"):
+                    # Web server returned a 404 error, send custom 404 Not Found page
+                    self.send_not_found_response()
+                else:
+                    # Cache the response and send it back to the client
+                    self.cache_response(url, response)
+                    self.send_response(response)
+            else:
+                # Web server returned an error, send 404 Not Found
+                self.send_not_found_response()
 
-        # Find the start and end of the image data in the response
-        start_index = response.find(b"\r\n\r\n") + 4
-        image_data = response[start_index:]
+    def handle_post_request(self, url, request, content_length):
+        request_body = self.receive_request_body(content_length)
+        response = self.forward_request("POST", url, request, request_body)
+        if response:
+            # Cache the response and send it back to the client
+            self.cache_response(url, response)
+            self.send_response(response)
+        else:
+            # Web server returned an error, send 404 Not Found
+            self.send_not_found_response()
 
-        # Return the image data
-        return image_data
-
-    except Exception as e:
-        # Handle any exceptions (e.g., connection error, invalid URL, etc.)
-        print(f"Error fetching image from {url}: {e}")
+    def handle_head_request(self, url):
+        response = self.forward_request(method, url, request)
+        if response:
+            self.send_response(response)
+        else:
+            # Web server returned an error, send 404 Not Found
+            self.send_not_found_response()      
+    
+    def get_cached_data(self, url):
+        # Check if the URL is cached and not expired, return cached data if available
+        # Otherwise, return None
         return None
 
-# Process the web request and return the response
-def process_request(client_socket, request_data):
-    # Your code to process the request here...
-    request_lines = request_data.split('\r\n')
-    request_line = request_lines[0]
-    method, url, _ = request_line.split(' ')
+    def cache_response(self, url, response):
+        # Cache the response data for the given URL
+        # Save the data in the cache directory with a unique filename
+        pass
 
-    if method not in ['GET', 'POST', 'HEAD']:
-        # Return 403 Forbidden for unsupported methods
-        response = "HTTP/1.1 403 Forbidden\r\nContent-Type: text/html\r\n\r\n<h1>403 Forbidden</h1>"
-        client_socket.sendall(response.encode())
-        client_socket.close()
-        return
+    def forward_request(self, method, url, request, body=None):
+        # Implement forwarding logic for GET, POST, and HEAD requests
+        # Use the 'socket' module to create a new connection to the web server
+        # Forward the request and receive the response from the web server
+        pass
 
-    if not is_whitelisted(url):
-        # Return 403 Forbidden for non-whitelisted URLs
-        response = "HTTP/1.1 403 Forbidden\r\nContent-Type: text/html\r\n\r\n<h1>403 Forbidden</h1>"
-        client_socket.sendall(response.encode())
-        client_socket.close()
-        return
+    def send_response(self, response):
+        # Send the received response from the web server back to the client
+        # Use the client socket to send the response data
+        pass
 
-    current_hour = int(time.strftime("%H"))
-    if current_hour < ALLOWED_START_TIME or current_hour >= ALLOWED_END_TIME:
-        # Return 403 Forbidden for requests outside the allowed time range
-        response = "HTTP/1.1 403 Forbidden\r\nContent-Type: text/html\r\n\r\n<h1>403 Forbidden</h1>"
-        client_socket.sendall(response.encode())
-        client_socket.close()
-        return
+    def send_cached_response(self, cached_data):
+        # Send the cached data back to the client as the response
+        # Use the client socket to send the response data
+        pass
 
-    if method == 'GET' and 'image' in url:
-        # Check cache for the image
-        image_path = os.path.join(CACHE_DIR, url)
-        if os.path.exists(image_path):
-            with open(image_path, 'rb') as f:
-                image_data = f.read()
-                response_headers = "HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\nConnection: Keep-alive\r\n\r\n"
-                client_socket.sendall(response_headers.encode() + image_data)
-            client_socket.close()
-            return
-        else:
-            # Fetch the image from the web server
-            image_data = fetch_image_from_web_server(url)
+    def send_forbidden_response(self):
+        # Respond with 403 Forbidden and the content of the custom 403.html page
+        response_content = b""
+        with open(FORBIDDEN_PAGE, "rb") as file:
+            response_content = file.read()
 
-            # Cache the image
-            save_cache(url, image_data)
+        response = b"HTTP/1.1 403 Forbidden\r\n"
+        response += b"Content-Type: text/html\r\n"
+        response += b"Content-Length: " + str(len(response_content)).encode() + b"\r\n"
+        response += b"\r\n"
+        response += response_content
 
-            # Send the image data as a response
-            response_headers = "HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\nConnection: Keep-alive\r\n\r\n"
-            client_socket.sendall(response_headers.encode() + image_data)
-            client_socket.close()
-            return
+        self.client_socket.sendall(response)
 
-    elif method == 'POST':
-        # Your code to handle POST requests here...
-        # This is just a simple example response for POST requests
-        response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>POST Request Received</h1>"
-        client_socket.sendall(response.encode())
-        client_socket.close()
-        return
+    def send_not_found_response(self):
+        # Respond with 404 Not Found and the content of the custom 404.html page
+        response_content = b""
+        with open(NOT_FOUND_PAGE, "rb") as file:
+            response_content = file.read()
 
-    elif method == 'HEAD':
-        # Your code to handle HEAD requests here...
-        # This is just a simple example response for HEAD requests
-        response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
-        client_socket.sendall(response.encode())
-        client_socket.close()
-        return
-    
-    # ... (code to handle other types of requests)
+        response = b"HTTP/1.1 200 Not Found\r\n"
+        response += b"Content-Type: text/html\r\n"
+        response += b"Content-Length: " + str(len(response_content)).encode() + b"\r\n"
+        response += b"\r\n"
+        response += response_content
 
-# Define the proxy server function to handle requests
-def proxy_server(client_socket, addr):
-    # Receive the request data from the client
-    request_data = client_socket.recv(4096).decode()
+        self.client_socket.sendall(response)
 
-    # Process the request and send the response
-    process_request(client_socket, request_data)
+def read_config():
+    # Read and parse the configuration file
+    # Implement configuration file parsing logic and return settings
+    pass
 
-# Main function to set up the proxy server
 def main():
-    # Load configurations from the config file
-    load_config()
+    if not os.path.exists(CACHE_DIRECTORY):
+        os.mkdir(CACHE_DIRECTORY)
 
-    # Load the cache from file
-    load_cache()
+    settings = read_config()
 
-    # Your setup logic for the proxy server here...
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((proxy_ip, proxy_port))
-    server_socket.listen(5)
-    print(f"Proxy server listening on {proxy_ip}:{proxy_port}")
+    proxy_host = "127.0.0.1"
+    proxy_port = 8888
 
-    while True:
-        client_socket, addr = server_socket.accept()
-        print(f"Received connection from {addr[0]}:{addr[1]}")
-        proxy_thread = threading.Thread(target=proxy_server, args=(client_socket, addr))
-        proxy_thread.start()
+    proxy_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    proxy_server.bind((proxy_host, proxy_port))
+    proxy_server.listen(5)
+
+    print(f"Proxy server is listening on {proxy_host}:{proxy_port}")
+
+    try:
+        while True:
+            client_socket, client_address = proxy_server.accept()
+            print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+            proxy_handler = ProxyHandler(client_socket, client_address)
+            threading.Thread(target=proxy_handler.handle_request).start()
+    except KeyboardInterrupt:
+        print("Shutting down proxy server")
+        proxy_server.close()
 
 if __name__ == "__main__":
     main()
