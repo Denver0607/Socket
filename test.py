b2956c451f03f1e1d454680a454f8bcbe20eaab6
LuciaNgo <ngotrieuman16052004@gmail.com>
2023-08-12 20:50:53 +0700

    download image and return exist image
    not done cleanup image yet

--- test.py
+++ test.py
@@ -31,11 +31,9 @@ settings = read_config()
 section = 'CONFIGURATION'
 TIME_OUT = int(settings[section]['time_out'][0])
 EXPIRATION_TIME = int(settings[section]['cache_time'][0])
+images_path = {}
 
 def receive_data(client_socket):
-    # print("receive_data: ")
-    # print(client_socket.getsockname)
-    # print(0)
     data = b''
     
     while b'\r\n\r\n' not in data:
@@ -51,16 +49,11 @@ def receive_data(client_socket):
     
         if content_length != 0:
             while content_length > len(body):
-                # body += receive_request_body(client_socket, content_length)
                 body += client_socket.recv(BUFSIZE)
                 
     # solve Connection: keep-alive (Transfer-Encoding)
     elif 'Transfer-Encoding' in headers:
         while b'\r\n\r\n' not in body:
-            # chunk = client_socket.recv(BUFSIZE)
-            # if len(chunk) == 0:
-            #     break
-            # body += chunk
             body += client_socket.recv(BUFSIZE)
             
     return first_line, headers, body
@@ -143,6 +136,23 @@ def send_not_found_response(client_socket):
 
     send_response(client_socket, response)
 
+def send_image_response(client_socket, file_path):
+    response_content = b""
+    with open(file_path, "rb") as file:
+        response_content = file.read()
+    
+    file_extension = os.path.splitext(file_path)
+    file_extension = file_extension[1].split('.')
+    content_type = "image/" + file_extension[1]
+
+    response = b"HTTP/1.1 200 OK\r\n"
+    response += b"Content-Type: " + content_type.encode() + b"\r\n"
+    response += b"Content-Length: " + str(len(response_content)).encode() + b"\r\n"
+    response += b"\r\n"
+    response += response_content
+
+    send_response(client_socket, response)
+
 def is_whitelisted(whitelisting, host_name):
     # Implement whitelisting logic from the config file
     # Return True if URL is whitelisted, otherwise False
@@ -172,6 +182,63 @@ def is_time_allowed(allowed_time):
         return True
     return False
 
+#done
+def make_file_path(host_name, url):
+    if not os.path.exists(CACHE_DIRECTORY):
+        os.mkdir(CACHE_DIRECTORY)
+
+    basename = host_name + '_' + os.path.basename(url) 
+    cache_filename = os.path.join(CACHE_DIRECTORY, basename)
+    return cache_filename
+
+# done
+def download_image(body, save_path):
+    try:
+        with open(save_path, 'wb') as f:
+            f.write(body)
+        print(f"Image downloaded and saved to {save_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+#done
+def is_cached_data(cache_filename):
+    try:
+        # Check if the data is cached and still valid
+        if os.path.exists(cache_filename):
+            # Get the last modified time of the cached file
+            cache_modified_time = os.path.getmtime(cache_filename)
+            current_time = datetime.datetime.now().timestamp()
+            # Calculate the age of the cached data in seconds
+            cache_age = current_time - cache_modified_time
+
+            # If the cached data is still valid, read and return it
+            if cache_age < EXPIRATION_TIME:
+                print("HAVE ALREADY EXISTED")
+                return True
+        else:
+            # print ("NOT YET")
+            return False
+
+    except Exception as e:
+        # Handle any potential errors when accessing or reading the cache file
+        print(f"Error while accessing cached data: {e}")
+        return False
+
+# not done
+def cleanup_expired_cache():
+    # start_time = time.time()  # Record the start time
+    # while time.time() - start_time < MAX_THREAD_RUNTIME:
+    while 1:
+        current_time = datetime.datetime.now().timestamp()
+        for cached_file in os.listdir(CACHE_DIRECTORY):
+            cached_file_path = os.path.join(CACHE_DIRECTORY, cached_file)
+            cache_modified_time = os.path.getmtime(cached_file_path)
+            if current_time - cache_modified_time > EXPIRATION_TIME:
+                # The cached data has expired, remove the cached file
+                os.remove(cached_file_path)
+                print(f"Expired cached file '{cached_file}' removed.")
+        time.sleep(1)  # Sleep for 1 seconds before the next iteration
+
 def make_message(first_line, headers, body):
     request = b''
     
@@ -191,38 +258,42 @@ def make_message(first_line, headers, body):
     
     return request
 
-def handle_request_message(client_socket, host_name, request):
+def handle_request_message(client_socket, host_name, request, save_path):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         server_socket.connect((host_name, PORT))
-        print('Successfully connected to ' + host_name)
+        # print('Successfully connected to ' + host_name)
     except socket.gaierror:
-        print("There was an error resolving the host")
+        print("There was an error connecting to the host")
         return
     
     try:
         server_socket.sendall(request)
-        response_count = 0
+        # response_count = 0
 
         while True:
             try:
                 server_socket.settimeout(TIME_OUT)
-                # response = server_socket.recv(BUFSIZE)
                 status, headers, body = receive_data(server_socket)
                 
                 if not status:
                     break
                 
-                response_count += 1
-                print('Response #: ' + str(response_count))
-                
+                # response_count += 1
+                # print('Response #: ' + str(response_count))
                 
                 if status[1] < '400':
                     response = make_message(status, headers, body)
                     send_response(client_socket, response)
-                    # client_socket.sendall(response)  # Send the response back to the client
+                    
+                    if 'Content-Type' in headers:
+                        if 'image' in headers['Content-Type']:
+                            print('this is an image')
+                            download_image(body, save_path)
+                            
                 elif status[1] == '404':
                     send_not_found_response(client_socket)
+                    
                 else:
                     send_forbidden_response(client_socket)
                     
@@ -241,14 +312,12 @@ def handle_request_message(client_socket, host_name, request):
             print("OSError:", e)
     
     finally:
-        print('Close connection to web server')
+        # print('Close connection to web server')
         server_socket.close()
 
 def handle_client(client_socket):
     try:
         request_count = 0
-        # current_host_name = ''
-        # web_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         while 1:
             try: 
                 client_socket.settimeout(TIME_OUT)
@@ -261,28 +330,17 @@ def handle_client(client_socket):
                 method = request_line[0]
                 url = request_line[1]
                 print(method + ' ' + url)
-                print()
-                host_name, path = extract_hostname_and_path(url)
+                # print()
+                host_name, __ = extract_hostname_and_path(url)
                 
                 if method in {'GET', 'POST', 'HEAD'} and is_whitelisted(settings[section]['whitelisting'], host_name) and is_time_allowed(settings[section]['time']):
-    
-                # if current_host_name != host_name:
-                #     if socket.isConneted(web_server):
-                #         print('Close connection to web server')
-                #         web_server.close()
-                #     current_host_name = host_name
-                    
-                    # try:
-                    #     web_server.connect((current_host_name, PORT))
-                    #     print('Successfully connected to ' + host_name)
-                    # except socket.gaierror:
-                    #     print("There was an error resolving the host")
-                    #     return
-                    # web_server.connect((current_host_name, PORT))
-                
-                    # handle_request_message(client_socket, host_name, request)
-                    THREAD = threading.Thread(target=handle_request_message, args=(client_socket, host_name, request))
-                    THREAD.start()
+                    file_path = make_file_path(host_name, url)
+                    if is_cached_data(file_path):
+                        send_image_response(client_socket, file_path)
+                        
+                    else:
+                        REQUEST_THREAD = threading.Thread(target=handle_request_message, args=(client_socket, host_name, request, file_path))
+                        REQUEST_THREAD.start()
                 else:
                     send_forbidden_response(client_socket)
                     
@@ -310,8 +368,8 @@ def accept_incoming_connections(proxy_server):
         print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
         # clients[client_socket] = client_socket
         print('Proxy is waiting for resquest')
-        THREAD = threading.Thread(target=handle_client, args=(client_socket,))
-        THREAD.start()
+        CLIENT_THREAD = threading.Thread(target=handle_client, args=(client_socket,))
+        CLIENT_THREAD.start()
         # handle_client(client_socket)
 
 def main():
